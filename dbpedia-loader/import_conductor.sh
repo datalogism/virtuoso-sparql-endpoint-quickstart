ba00c5a5edec95439777b43f33b959761f54e69e
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-06-10 11:58:30 +0200

    Update import_conductor.sh

--- dbpedia-loader/import_conductor.sh
+++ dbpedia-loader/import_conductor.sh
@@ -72,14 +72,6 @@ fi
 
 /bin/bash ./process/structure_process.sh
 
-## CREATE SUBGRAPHS
-run_virtuoso_cmd "DB.DBA.RDF_GRAPH_GROUP_CREATE ('${DOMAIN}',1);"
-run_virtuoso_cmd "DB.DBA.RDF_GRAPH_GROUP_INS ('${DOMAIN}','${DOMAIN}/graph/metadata');"
-
-
-echo "[INFO] ADD META DATA"
-run_virtuoso_cmd "DB.DBA.TTLP_MT (file_to_string_output ('${STORE_DATA_DIR}/meta_base/dbpedia_fr-metadata.ttl'), '', '${DOMAIN}/graph/metadata');" 
-
 echo "[INFO] Setting 'dbp_decode_iri' registry entry to 'on'"
 run_virtuoso_cmd "registry_set ('dbp_decode_iri', 'on');"
 
