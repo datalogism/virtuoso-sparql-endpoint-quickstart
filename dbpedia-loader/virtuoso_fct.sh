0200899bd23ac16166bc173407eb78c4704cfd91
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-06-10 15:06:39 +0200

    Create virtuoso_fct.sh

---
+++ dbpedia-loader/virtuoso_fct.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+bin="isql-vt"
+host="store"
+port=$STORE_ISQL_PORT
+user="dba"
+
+run_virtuoso_cmd () {
+ VIRT_OUTPUT=`echo "$1" | "$bin" -H "$host" -S "$port" -U "$user" -P "$STORE_DBA_PASSWORD" 2>&1`
+ VIRT_RETCODE=$?
+ if [[ $VIRT_RETCODE -eq 0 ]]; then
+   echo "$VIRT_OUTPUT" | tail -n+5 | perl -pe 's|^SQL> ||g'
+   return 0
+ else
+   echo -e "[ERROR] running the these commands in virtuoso:\n$1\nerror code: $VIRT_RETCODE\noutput:"
+   echo "$VIRT_OUTPUT"
+   let 'ret = VIRT_RETCODE + 128'
+   return $ret
+ fi
+}
+
+wait_for_download() {
+  sleep 100
+  while [ -f "${DATA_DIR}/download.lck" ]; do
+    sleep 1
+  done
+}
+
+test_connection () {
+   if [[ -z $1 ]]; then
+       echo "[ERROR] missing argument: retry attempts"
+       exit 1
+   fi
+
+   t=$1
+
+   run_virtuoso_cmd 'status();'
+   while [[ $? -ne 0 ]] ;
+   do
+       echo -n "."
+       sleep 1
+       echo $t
+       let "t=$t-1"
+       if [ $t -eq 0 ]
+       then
+           echo "timeout"
+           return 2
+       fi
+       run_virtuoso_cmd 'status();'
+   done
+}
