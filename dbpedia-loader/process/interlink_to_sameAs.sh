e888191c9f3fe74fabcf8e353afa1f3f0d1d7825
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-06-13 11:27:15 +0200

    Update interlink_to_sameAs.sh

--- dbpedia-loader/process/interlink_to_sameAs.sh
+++ dbpedia-loader/process/interlink_to_sameAs.sh
@@ -1,5 +1,6 @@
 . ../virtuoso_fct.sh --source-only
 
+################### SPARQL - GET ALL THE SAMEAS LINKS
 get_named_graph='SPARQL \
 SELECT ?o FROM <http://fr.dbpedia.org/graph/metadata> WHERE {\
 ?s sd:namedGraph ?o.\
@@ -12,6 +13,8 @@ resp=$(run_virtuoso_cmd "$get_named_graph");
 graph_list=$(echo $resp | tr " " "\n" | grep -E "\/graph\/");
 echo "> INTERLANG LINKS TRANSFORM TO SAMEAS ";
 nbsameAs=0;
+
+################### SPARQL - COUNT INTERLINKS
 resp_interlang=$(run_virtuoso_cmd "SPARQL \
 SELECT count(?s) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_interlanguage-links> WHERE {\
 ?s dbo:wikiPageInterLanguageLink ?o \
@@ -21,6 +24,8 @@ if [ $nb_interlang -ne 0 ]
 then
     while [ $nb_interlang -ne $nbsameAs ];
     do
+
+        ################### SPARQL - ADD SAMEAS LINKS FOR EACH INTERLANG
         resp_update=$(run_virtuoso_cmd "SPARQL \
         WITH <http://fr.dbpedia.org/graph/dbpedia_generic_interlanguage-links>\
         INSERT { ?x owl:sameAs ?y } WHERE {\
@@ -29,6 +34,8 @@ then
         MINUS{ ?x owl:sameAs ?y }\
         } LIMIT $limit \
         } ;");
+
+        ################### SPARQL - COUNT INTERLANG TO TRANSFORM
         resp_sameAs=$(run_virtuoso_cmd "SPARQL\
         SELECT count(?s) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_interlanguage-links> WHERE { \
         ?s owl:sameAs ?o\
