d04680c64431b9b12a81693dd8558e92e9943da8
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-06-13 17:21:29 +0200

    Update multilingual_labels.sh

--- dbpedia-loader/process/multilingual_labels.sh
+++ dbpedia-loader/process/multilingual_labels.sh
@@ -11,5 +11,37 @@ resp=$(run_virtuoso_cmd "$get_lang_labels");
 lang_list=$(echo $resp | tr " " "\n");
 for lang in ${lang_list[@]}; do
   echo "$lang need to be treaten";
-  
+  resp_todo=$(run_virtuoso_cmd "SPARQL SELECT DISTINCT COUNT(?s_lang) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels>  WHERE {\
+  ?s_lang rdfs:label ?o_lang.\
+  FILTER(lang(?o_lang)='$lang').\
+  {\
+  SELECT ?s_fr ?s_lang FROM <http://fr.dbpedia.org/graph/dbpedia_generic_interlanguage-links> WHERE { ?s_fr owl:sameAs ?s_lang }\
+  }\
+  } ;");
+  nb_wikilinks=$(echo $resp_todo | awk '{print $4}');
+  while [ $nb_wikilinks -ne 0 ]
+  do
+    resp_labels=$(run_virtuoso_cmd "SPARQL\
+	    DEFINE sql:log-enable 2  WITH  <http://fr.dbpedia.org/graph/dbpedia_generic_labels>\
+	    DELETE { ?s_lang rdfs:label ?o_lang. }\
+	    INSERT { ?s_fr rdfs:label ?o_lang. }\
+	    WHERE {  \
+      SELECT ?s_fr ?o_lang ?s_lang FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels>  WHERE {\
+      ?s_lang rdfs:label ?o_lang.\
+      FILTER(lang(?o_lang)='$lang').\
+      {\
+        SELECT ?s_fr ?s_lang FROM <http://fr.dbpedia.org/graph/dbpedia_generic_interlanguage-links> WHERE { ?s_fr owl:sameAs ?s_lang }\
+      }\
+      }  LIMIT $limit };"); 
+      
+      resp_todo=$(run_virtuoso_cmd "SPARQL SELECT DISTINCT COUNT(?s_lang) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels>  WHERE {\
+      ?s_lang rdfs:label ?o_lang.\
+      FILTER(lang(?o_lang)='$lang').\
+      {\
+      SELECT ?s_fr ?s_lang FROM <http://fr.dbpedia.org/graph/dbpedia_generic_interlanguage-links> WHERE { ?s_fr owl:sameAs ?s_lang }\
+      }\
+      } ;");
+      nb_wikilinks=$(echo $resp_todo | awk '{print $4}');
+      echo $nb_wikilinks;
+  done
 done
