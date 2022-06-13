9dbf324344ce93c20aa27387386a3fd9181faa19
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-06-13 13:43:30 +0200

    Create process_wikidata.sh

---
+++ dbpedia-loader/process/process_wikidata.sh
@@ -0,0 +1,59 @@
+. ../virtuoso_fct.sh --source-only
+
+get_named_graph='SPARQL SELECT ?o FROM <http://fr.dbpedia.org/graph/metadata> WHERE { ?s sd:namedGraph ?o. FILTER( ?o != <http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis> AND ?o != <http://fr.dbpedia.org/graph/dbpedia_generic_interlanguage-links> AND STRSTARTS(STR(?o), "http://fr.dbpedia.org/graph/dbpedia_wikidata_"))};'
+resp=$(run_virtuoso_cmd "$get_named_graph");
+graph_list=$(echo $resp | tr " " "\n" | grep -E "\/graph\/");
+
+echo "> ADD FLAG AND PROPAGATE CHANGE"
+count=0;
+nb_global=1;
+last=0;
+while [ $nb_global -ne $last ]
+do
+    echo "NEW LOOP $nb_global not equals to  $last" ;
+    last=$nb_global;
+    resp2=$(run_virtuoso_cmd "SPARQL DEFINE sql:log-enable 2 WITH <http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis>  INSERT { ?y rdf:type dbo:frResource. } WHERE { SELECT ?y FROM <http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis> WHERE { ?s owl:sameAs ?y. FILTER NOT EXISTS { ?y rdf:type dbo:frResource }. FILTER(STRSTARTS(STR(?y), 'http://fr.dbpedia.org/') ) } LIMIT $limit};");
+    resp_count=$(run_virtuoso_cmd "SPARQL SELECT COUNT(?s) FROM  <http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis> WHERE { ?s rdf:type dbo:frResource };");
+    nb_global=$(echo $resp_count | awk '{print $4}');
+    echo ">>>>>> UPDATE EACH GRAPH SUBJECTS";
+    for graph in ${graph_list[@]}; do
+        nb_todo0=1;
+        while [ $nb_todo0 -ne 0 ]
+        do
+            resp_updategraph=$(run_virtuoso_cmd "SPARQL DEFINE sql:log-enable 2  WITH <$graph> DELETE { ?y ?p ?o. } INSERT { ?s ?p ?o. } WHERE { SELECT ?s ?p ?o ?y WHERE {{SELECT ?s ?y FROM <http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis> WHERE { ?y owl:sameAs ?s. FILTER EXISTS { ?s rdf:type  dbo:frResource }} } . {SELECT ?y ?p ?o FROM <$graph> WHERE {?y ?p ?o } } }  LIMIT $limit };");
+            resp_todo0=$(run_virtuoso_cmd "SPARQL SELECT COUNT(?y) WHERE {{SELECT ?s ?y FROM <http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis> WHERE { ?y owl:sameAs ?s. FILTER EXISTS { ?s rdf:type  dbo:frResource }} } . {SELECT ?y ?p ?o FROM <$graph> WHERE {?y ?p ?o } } };");
+            nb_todo0=$(echo $resp_todo0 | awk '{print $4}');
+            echo "$graph need to change Subjects : $nb_todo0";
+        done
+    done
+    echo ">>>>>> UPDATE EACH GRAPH OBJECTS";
+    for graph in ${graph_list[@]}; do
+        nb_todo0=1;
+        while [ $nb_todo0 -ne 0 ]
+        do
+            resp_updategraph=$(run_virtuoso_cmd "SPARQL DEFINE sql:log-enable 2  WITH <$graph> DELETE { ?s ?p ?wkd. } INSERT { ?s ?p ?dbfr. } WHERE {SELECT ?dbfr ?p ?s ?wkd WHERE {{SELECT ?dbfr ?wkd FROM <http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis> WHERE { ?wkd owl:sameAs ?dbfr. FILTER EXISTS { ?dbfr rdf:type  dbo:frResource }} } . {SELECT ?s ?p ?wkd FROM <$graph> WHERE {?s ?p ?wkd } } }  LIMIT $limit };");
+			resp_todo0=$(run_virtuoso_cmd "SPARQL SELECT COUNT(?wkd) WHERE {{SELECT ?dbfr ?wkd FROM <http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis> WHERE { ?wkd owl:sameAs ?dbfr. FILTER EXISTS { ?dbfr rdf:type  dbo:frResource }} } . {SELECT ?s ?p ?wkd FROM <$graph> WHERE {?s ?p ?wkd } } };");
+			nb_todo0=$(echo $resp_todo0 | awk '{print $4}');
+			echo "$graph need to change objects : $nb_todo0";
+        done
+    done
+    resp3=$(run_virtuoso_cmd "SPARQL DEFINE sql:log-enable 2 WITH <http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis>  INSERT { ?y owl:sameAs ?s. } WHERE { SELECT ?y ?s FROM <http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis> WHERE { ?s owl:sameAs ?y. FILTER EXISTS { ?y rdf:type dbo:frResource }} LIMIT $limit};");
+    echo ">>>>>> LINK TO FR RESSOURCE";
+    nb_todo=1;
+    while [ $nb_todo -ne 0 ]
+    do
+        resp4=$(run_virtuoso_cmd "SPARQL DEFINE sql:log-enable 2 WITH <http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis>  DELETE { ?s owl:sameAs ?p. } INSERT { ?y owl:sameAs ?p. } WHERE { SELECT ?s ?y ?p FROM <http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis> WHERE {?y rdf:type dbo:frResource. ?s owl:sameAs ?y. ?s owl:sameAs ?p. FILTER (?y != ?p ) } LIMIT $limit };");
+        resp_todo=$(run_virtuoso_cmd "SPARQL SELECT COUNT(*) FROM <http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis> WHERE {?y rdf:type dbo:frResource. ?s owl:sameAs ?y. ?s owl:sameAs ?p. FILTER (?y != ?p ) };");
+        nb_todo=$(echo $resp_todo | awk '{print $4}');
+        echo $nb_todo;
+    done
+    echo ">>>>>> INVERSE SAME AS"
+    nb_todo2=1;
+    while [ $nb_todo2 -ne 0 ]
+    do
+        resp5=$(run_virtuoso_cmd "SPARQL DEFINE sql:log-enable 2 WITH <http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis>  DELETE { ?s owl:sameAs ?y. } INSERT { ?y owl:sameAs ?s. } WHERE { SELECT ?y ?s FROM <http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis> WHERE {?y rdf:type dbo:frResource. ?s owl:sameAs ?y } LIMIT $limit };");
+        resp_todo2=$(run_virtuoso_cmd "SPARQL SELECT COUNT(?s) FROM <http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis> WHERE {?y rdf:type dbo:frResource. ?s owl:sameAs ?y. };");
+        nb_todo2=$(echo $resp_todo2 | awk '{print $4}');
+    done
+done
+echo "[CLEAN WIKIDATA] END";
