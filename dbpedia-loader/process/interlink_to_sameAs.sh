e92cff5a9ff31f9ccf7e3c2768c7bc8644293a79
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-06-13 13:47:47 +0200

    Update interlink_to_sameAs.sh

--- dbpedia-loader/process/interlink_to_sameAs.sh
+++ dbpedia-loader/process/interlink_to_sameAs.sh
@@ -1,16 +1,7 @@
 . ../virtuoso_fct.sh --source-only
+#!/usr/bin/env bash
+. ../virtuoso_fct.sh --source-only
 
-################### SPARQL - GET ALL THE SAMEAS LINKS
-get_named_graph='SPARQL \
-SELECT ?o FROM <http://fr.dbpedia.org/graph/metadata> WHERE {\
-?s sd:namedGraph ?o.\
-FILTER( ?o != <http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis> \
-AND ?o != <http://fr.dbpedia.org/graph/dbpedia_generic_interlanguage-links>\
-AND STRSTARTS(STR(?o), "http://fr.dbpedia.org/graph/dbpedia_wikidata_")\
-)\
-};'
-resp=$(run_virtuoso_cmd "$get_named_graph");
-graph_list=$(echo $resp | tr " " "\n" | grep -E "\/graph\/");
 echo "> INTERLANG LINKS TRANSFORM TO SAMEAS ";
 nbsameAs=0;
 
