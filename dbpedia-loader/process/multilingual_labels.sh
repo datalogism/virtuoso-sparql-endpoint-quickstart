26343b73135bf556c25db045d5c9d5d6434f4602
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-06-14 10:32:38 +0200

    Update multilingual_labels.sh

--- dbpedia-loader/process/multilingual_labels.sh
+++ dbpedia-loader/process/multilingual_labels.sh
@@ -15,6 +15,8 @@ lang_list=$(echo $resp | tr " " "\n" | grep -oP "lang_\K(.*)");
 for lang in ${lang_list[@]}; do
     if [[ $lang != 'fr' ]]; then
 	  echo "$lang need to be treaten";
+	  
+          ################### SPARQL - COUNT LANG TO DO
 	  resp_todo=$(run_virtuoso_cmd "SPARQL SELECT DISTINCT COUNT(?s_lang) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels>  WHERE {\
 	  ?s_lang rdfs:label ?o_lang.\
 	  FILTER(lang(?o_lang)='$lang').\
@@ -25,7 +27,9 @@ for lang in ${lang_list[@]}; do
 	  nb_wikilinks=$(echo $resp_todo | awk '{print $4}');
 	  while [ $nb_wikilinks -ne 0 ]
 	  do
-	    resp_labels=$(run_virtuoso_cmd "SPARQL\
+	  
+            ################### SPARQL - ATTACH TRANSLATION TO FRENCH LABEL
+	     resp_labels=$(run_virtuoso_cmd "SPARQL\
 		    DEFINE sql:log-enable 2  WITH  <http://fr.dbpedia.org/graph/dbpedia_generic_labels>\
 		    DELETE { ?s_lang rdfs:label ?o_lang. }\
 		    INSERT { ?s_fr rdfs:label ?o_lang. }\
@@ -38,6 +42,7 @@ for lang in ${lang_list[@]}; do
 	      }\
 	      }  LIMIT $limit };"); 
 
+              ################### SPARQL - COUNT AGAIN HOW MANY WE NEED TO DO
 	      resp_todo=$(run_virtuoso_cmd "SPARQL SELECT DISTINCT COUNT(?s_lang) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels>  WHERE {\
 	      ?s_lang rdfs:label ?o_lang.\
 	      FILTER(lang(?o_lang)='$lang').\
@@ -49,4 +54,34 @@ for lang in ${lang_list[@]}; do
 	      echo $nb_wikilinks;
 	  done
     fi
+done
+
+ ################### SPARQL - HOW MANY WE NEED TO DELETE ?
+resp2=$(run_virtuoso_cmd "SPARQL\
+DEFINE sql:log-enable 2\
+SELECT COUNT(distinct ?s) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels>  where {
+?s ?p ?o.
+FILTER(!STRSTARTS(STR(?s), 'http://fr.dbpedia.org/') )
+};");
+nb_to_delete=$(echo $resp2 | awk '{print $4}');
+while [ $nb_to_delete -ne 0 ]
+do
+	
+ 	################### SPARQL - DELETE LABELS THAT IS NOT RELATED TO FRENCH ENTITY
+        resp2=$(run_virtuoso_cmd "SPARQL\
+	DEFINE sql:log-enable 2  WITH  <http://fr.dbpedia.org/graph/dbpedia_generic_labels>\
+	DELETE { ?s ?p ?o. }\
+	SELECT ?s ?p ?o FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels>  where {
+	?s ?p ?o.
+	FILTER(!STRSTARTS(STR(?s), 'http://fr.dbpedia.org/') )
+	};");
+	
+ 	################### SPARQL - HOW MANY WE NEED TO DELETE ?
+	resp2=$(run_virtuoso_cmd "SPARQL\
+	DEFINE sql:log-enable 2\
+	SELECT COUNT(distinct ?s) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels>  where {
+	?s ?p ?o.
+	FILTER(!STRSTARTS(STR(?s), 'http://fr.dbpedia.org/') )
+	};");
+	nb_to_delete=$(echo $resp2 | awk '{print $4}');
 done
