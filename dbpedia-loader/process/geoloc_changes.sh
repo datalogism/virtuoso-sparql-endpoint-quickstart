0d9771178ddf0144fa371fe74dd5ca8445201401
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-06-24 15:53:41 +0200

    Update geoloc_changes.sh

--- dbpedia-loader/process/geoloc_changes.sh
+++ dbpedia-loader/process/geoloc_changes.sh
@@ -3,40 +3,31 @@
 limit=500000;
 
 echo "=============> GEOLOC CHANGES NEW 3"
-resp_georelated=$(run_virtuoso_cmd "SPARQL \
-SELECT count(?o) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_geo-coordinates> WHERE { \
-?s ?p ?o. FILTER (!isBlank(?o)) \
-};");
 
-nb_todo=$(get_answer_nb "$resp_georelated");
-
+nb_last=0
+nb_new=1
 echo "=============> NB TODO : $nb_todo";
-while [ $nb_todo -ne 0 ];
+while [ $nb_new -gt $nb_last ];
     do
 
  ################### SPARQL - ADD BLANK NODE FOR EACH RELATED PLACE
      resp_update=$(run_virtuoso_cmd "SPARQL \
      WITH <http://fr.dbpedia.org/graph/dbpedia_generic_geo-coordinates> \
-     DELETE {  ?s <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ?type; \
-      <http://www.georss.org/georss/point> ?point; \
-      <http://www.w3.org/2003/01/geo/wgs84_pos#lat> ?lat; \ 
-      <http://www.w3.org/2003/01/geo/wgs84_pos#long> ?long; \ 
-      <http://www.w3.org/2003/01/geo/wgs84_pos#geometry> ?geo. \
+     DELETE {  ?s ?p ?o. \
      } \
      INSERT { ?s dbo:relatedPlaces [ \
-     <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ?type; \
-      <http://www.georss.org/georss/point> ?point; \
-      <http://www.w3.org/2003/01/geo/wgs84_pos#lat> ?lat; \
-      <http://www.w3.org/2003/01/geo/wgs84_pos#long> ?long; \
-     <http://www.w3.org/2003/01/geo/wgs84_pos#geometry> ?geo; \
+         <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ?type; \
+         <http://www.georss.org/georss/point> ?point; \
+         <http://www.w3.org/2003/01/geo/wgs84_pos#lat> ?lat; \
+         <http://www.w3.org/2003/01/geo/wgs84_pos#long> ?long; \
+         <http://www.w3.org/2003/01/geo/wgs84_pos#geometry> ?geo; \
      ]. \ 
      } \
     WHERE { \
-     SELECT ?s ?type ?geo ?lat ?long ?point \
+     SELECT ?s ?p ?o ?type ?geo ?lat ?long ?point \
      FROM <http://fr.dbpedia.org/graph/dbpedia_generic_geo-coordinates> \
      WHERE { \
-     ?s ?p ?o. \
-     OPTIONAL{ ?s <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ?type .} \
+     ?s <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ?type . \
      OPTIONAL{ ?s <http://www.georss.org/georss/point> ?point .} \
      OPTIONAL { ?s <http://www.w3.org/2003/01/geo/wgs84_pos#lat> ?lat.} \
      OPTIONAL { ?s <http://www.w3.org/2003/01/geo/wgs84_pos#long> ?long.} \
@@ -45,10 +36,10 @@ while [ $nb_todo -ne 0 ];
      } ;");
      
      
+     nb_last=$nb_new;
      echo "=============> update";
-     resp_georelated2=$(run_virtuoso_cmd "SPARQL SELECT count(?o) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_geo-coordinates> WHERE { ?s ?p ?o. FILTER (!isBlank(?o)) };");
-     nb_todo=$(get_answer_nb "$resp_georelated2");
-     
+     resp_georelated2=$(run_virtuoso_cmd "SPARQL SELECT COUNT(?s) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_geo-coordinates> WHERE {?s dbo:relatedPlaces ?o. } ;");
+     nb_new=$(get_answer_nb "$resp_georelated2");
      echo "=============> NB TODO : $nb_todo";
 done
 
