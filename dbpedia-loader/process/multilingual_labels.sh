4650c7c1f386a8d9606a8f087d19bce2e0fc44f1
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-07-04 11:54:20 +0200

    Update multilingual_labels.sh

--- dbpedia-loader/process/multilingual_labels.sh
+++ dbpedia-loader/process/multilingual_labels.sh
@@ -5,10 +5,7 @@ limit=500000;
 echo "============ multilingual_labels.sh"
 ################### SPARQL - GET LANG LIST
 resp=$(run_virtuoso_cmd "SPARQL \
-SELECT DISTINCT CONCAT('lang_',?lang) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels> where { \
-?s rdfs:label ?o.\
-BIND (lang(?o) AS ?lang) \
-};";);
+SELECT DISTINCT CONCAT('lang_',?lang) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels> where { ?s rdfs:label ?o. BIND (lang(?o) AS ?lang) };";);
 echo $resp;
 lang_list=$(echo $resp | tr " " "\n" | grep -oP "lang_\K(.*)");
 
@@ -44,24 +41,12 @@ for lang in ${lang_list[@]}; do
 	      }  LIMIT $limit };"); 
 
               ################### SPARQL - COUNT AGAIN HOW MANY WE NEED TO DO O VIA WIKILINKS
-	      resp_todo=$(run_virtuoso_cmd "SPARQL SELECT DISTINCT COUNT(?s_lang) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels>  WHERE { \
-	      ?s_lang rdfs:label ?o_lang. \
-	      FILTER(lang(?o_lang)='$lang'). \
-	      { \
-	      SELECT ?s_fr ?s_lang FROM <http://fr.dbpedia.org/graph/dbpedia_generic_interlanguage-links> WHERE { ?s_fr owl:sameAs ?s_lang } \
-	      } \
-	      } ;");
+	      resp_todo=$(run_virtuoso_cmd "SPARQL SELECT DISTINCT COUNT(?s_lang) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels>  WHERE { ?s_lang rdfs:label ?o_lang. FILTER(lang(?o_lang)='$lang'). { SELECT ?s_fr ?s_lang FROM <http://fr.dbpedia.org/graph/dbpedia_generic_interlanguage-links> WHERE { ?s_fr owl:sameAs ?s_lang } } } ;");
     	      nb_wikilinks=$(get_answer_nb "$resp_todo");
 	      echo $nb_wikilinks;
 	  done
 	  ################### SPARQL - COUNT LANG TO DO VIA WIKIDATA
-	  resp_todo=$(run_virtuoso_cmd "SPARQL SELECT DISTINCT COUNT(?s_lang) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels>  WHERE { \
-	  ?s_lang rdfs:label ?o_lang. \
-	  FILTER(lang(?o_lang)='$lang'). \
-	  { \
-	  SELECT ?s_fr ?s_lang FROM <http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis> WHERE { ?s_fr owl:sameAs ?s_lang } \
-	  } \
-	  } ;");
+	  resp_todo=$(run_virtuoso_cmd "SPARQL SELECT DISTINCT COUNT(?s_lang) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels>  WHERE { ?s_lang rdfs:label ?o_lang. FILTER(lang(?o_lang)='$lang'). { SELECT ?s_fr ?s_lang FROM <http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis> WHERE { ?s_fr owl:sameAs ?s_lang } } } ;");
 	  
     	  nb_wikidata=$(get_answer_nb "$resp_todo");
 	  while [ $nb_wikidata -ne 0 ]
@@ -82,13 +67,7 @@ for lang in ${lang_list[@]}; do
 	      }  LIMIT $limit };"); 
 
               ################### SPARQL - COUNT AGAIN HOW MANY WE NEED TO DO  VIA WIKIDATA
-	      resp_todo=$(run_virtuoso_cmd "SPARQL SELECT DISTINCT COUNT(?s_lang) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels>  WHERE { \
-	      ?s_lang rdfs:label ?o_lang. \
-	      FILTER(lang(?o_lang)='$lang'). \
-	      { \
-	      SELECT ?s_fr ?s_lang FROM <http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis> WHERE { ?s_fr owl:sameAs ?s_lang } \
-	      } \
-	      } ;");
+	      resp_todo=$(run_virtuoso_cmd "SPARQL SELECT DISTINCT COUNT(?s_lang) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels>  WHERE { ?s_lang rdfs:label ?o_lang. FILTER(lang(?o_lang)='$lang'). { SELECT ?s_fr ?s_lang FROM <http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis> WHERE { ?s_fr owl:sameAs ?s_lang } } } ;");
     	      nb_wikidata=$(get_answer_nb "$resp_todo");
 	      echo $nb_wikidata;
 	  done
@@ -96,12 +75,7 @@ for lang in ${lang_list[@]}; do
 done
 
  ################### SPARQL - HOW MANY WE NEED TO DELETE ?
-resp2=$(run_virtuoso_cmd "SPARQL\
-DEFINE sql:log-enable 2\
-SELECT COUNT(distinct ?s) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels>  where {
-?s ?p ?o.
-FILTER(!STRSTARTS(STR(?s), 'http://fr.dbpedia.org/') )
-};");
+resp2=$(run_virtuoso_cmd "SPARQL SELECT COUNT(distinct ?s) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels>  where { ?s ?p ?o. FILTER(!STRSTARTS(STR(?s), 'http://fr.dbpedia.org/') ) };");
 nb_to_delete=$(get_answer_nb "$resp2");
 while [ $nb_to_delete -ne 0 ]
 do
@@ -116,12 +90,7 @@ do
 	};");
 	
  	################### SPARQL - HOW MANY WE NEED TO DELETE ?
-	resp2=$(run_virtuoso_cmd "SPARQL\
-	DEFINE sql:log-enable 2\
-	SELECT COUNT(distinct ?s) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels>  where {
-	?s ?p ?o.
-	FILTER(!STRSTARTS(STR(?s), 'http://fr.dbpedia.org/') )
-	};");
+	resp2=$(run_virtuoso_cmd "SPARQL SELECT COUNT(distinct ?s) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels>  where { ?s ?p ?o. FILTER(!STRSTARTS(STR(?s), 'http://fr.dbpedia.org/') )};");
 	
         nb_to_delete=$(get_answer_nb "$resp2");
 done
