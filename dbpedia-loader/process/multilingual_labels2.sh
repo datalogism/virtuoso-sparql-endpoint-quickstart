b9b95315e6282461668ffe1577871d012d617bcb
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-07-05 16:46:05 +0200

    Update multilingual_labels2.sh

--- dbpedia-loader/process/multilingual_labels2.sh
+++ dbpedia-loader/process/multilingual_labels2.sh
@@ -3,7 +3,7 @@
 
 limit=500000;
 
-echo "============ multilingual_labels2.sh V3 "
+echo "============ multilingual_labels2.sh V4 "
 ################### SPARQL - GET LANG LIST
 resp=$(run_virtuoso_cmd "SPARQL SELECT DISTINCT CONCAT('lang_',?lang) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels> where { ?s rdfs:label ?o. BIND (lang(?o) AS ?lang) };";);
 echo $resp;
@@ -32,6 +32,7 @@ for lang in ${lang_list[@]}; do
 		last_wkd=0;
 		while [ $nb_global_wkd -ne $last_wkd ]
 		do
+			last_wlk=$nb_global_wkd;
 			resp_wikilinks_flag=$(run_virtuoso_cmd "SPARQL  WITH <http://fr.dbpedia.org/graph/dbpedia_generic_labels> DELETE { ?s_lang rdfs:label ?o_lang. } INSERT { ?s_fr rdf:type dbo:${Lang}FrResource. ?s_fr rdfs:label ?o_lang.} WHERE {SELECT  ?s_fr ?s_lang ?o_lang  FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels>  WHERE {?s_lang rdfs:label ?o_lang. {SELECT ?s_fr ?s_lang FROM <http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis> WHERE { ?s_fr owl:sameAs ?s_lang. ?s_fr rdf:type  dbo:frResource } } . FILTER NOT EXISTS { ?s_fr rdf:type  dbo:${Lang}FrResource } . FILTER(lang(?o_lang)='$lang') } LIMIT $limit };");
 			resp_count=$(run_virtuoso_cmd "SPARQL SELECT COUNT(?s_fr) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels> WHERE { ?s_fr rdf:type dbo:${Lang}FrResource };");
 			nb_global_wlk=$(get_answer_nb "$resp_count");
