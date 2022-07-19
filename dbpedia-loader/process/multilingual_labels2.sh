70d4c4b4c899630f0efea0cd27e0753b19d813b5
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-07-19 16:44:41 +0200

    Update multilingual_labels2.sh

--- dbpedia-loader/process/multilingual_labels2.sh
+++ dbpedia-loader/process/multilingual_labels2.sh
@@ -3,12 +3,11 @@
 
 limit=500000;
 
-echo "============ multilingual_labels2.sh V5 "
+echo "============ multilingual_labels2.sh V6 "
 ################### SPARQL - GET LANG LIST
 resp=$(run_virtuoso_cmd "SPARQL SELECT DISTINCT CONCAT('lang_',?lang) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels> where { ?s rdfs:label ?o. BIND (lang(?o) AS ?lang) };";);
 echo $resp;
 lang_list=$(echo $resp | tr " " "\n" | grep -oP "lang_\K(.*)");
-#graph_list=("http://fr.dbpedia.org/graph/dbpedia_generic_interlanguage-links" "http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis")
 
 for lang in ${lang_list[@]}; do
 	if [[ $lang != 'fr' ]]; then
@@ -41,3 +40,19 @@ for lang in ${lang_list[@]}; do
 		
 	fi 
 done
+
+echo "============>>>>>>>>>> DELETE NOT USED LABELS"; 
+resp_count=$(run_virtuoso_cmd "SPARQL SELECT COUNT(?S) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels> WHERE { ?S ?p ?o . FILTER NOT EXISTS { ?S a ?t } };" 
+nb_todelete=$(get_answer_nb "$resp_count");
+while [ $nb_todelete -ne 0 ]
+do
+	$(run_virtuoso_cmd "SPARQL \
+       DEFINE sql:log-enable 2  WITH <http://fr.dbpedia.org/graph/dbpedia_generic_labels> \
+       DELETE { ?S ?p ?o. } \
+       WHERE {   \
+       SELECT ?S ?p ?o FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels> WHERE { \
+       ?S ?p ?o . FILTER NOT EXISTS { ?S a ?t } }  LIMIT $limit }};");
+       resp_count=$(run_virtuoso_cmd "SPARQL SELECT COUNT(?S) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels> WHERE { ?S ?p ?o . FILTER NOT EXISTS { ?S a ?t } };" 
+       nb_todelete=$(get_answer_nb "$resp_count");
+done
+
