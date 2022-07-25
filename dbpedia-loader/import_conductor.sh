a4b5edb511f5850f32977c8ce8aad422d1c206fa
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-07-25 10:05:30 +0200

    Update import_conductor.sh

--- dbpedia-loader/import_conductor.sh
+++ dbpedia-loader/import_conductor.sh
@@ -3,7 +3,7 @@
 . ./virtuoso_fct.sh --source-only
 
 #### PROCESS COMMANDS 
-if [ -z ${PROCESS_STRUCTURE+x} ]; then PROCESS_STRUCTURE=1; fi
+if [ -z ${PROCESS_INIT+x} ]; then PROCESS_INIT=1; fi
 if [ -z ${PROCESS_GEOLOC+x} ]; then PROCESS_GEOLOC=1; fi
 if [ -z ${PROCESS_INTERLINKSAMEAS+x} ]; then PROCESS_INTERLINKSAMEAS=1; fi
 if [ -z ${PROCESS_WIKIDATA+x} ]; then PROCESS_WIKIDATA=1; fi
@@ -15,7 +15,7 @@ echo "==========================================";
 echo " DBPEDIA LOADER VERSION of 19/07/2022";
 echo "==========================================";
 echo "------------ Current config ------------";
-echo "> PROCESS_STRUCTURE : ${PROCESS_STRUCTURE}";
+echo "> PROCESS_INIT: ${PROCESS_INIT}";
 echo "> PROCESS_GEOLOC : ${PROCESS_GEOLOC}";
 echo "> PROCESS_INTERLINKSAMEAS : ${PROCESS_INTERLINKSAMEAS}";
 echo "> PROCESS_WIKIDATA : ${PROCESS_WIKIDATA}";
@@ -47,62 +47,20 @@ if [ $? -eq 2 ]; then
 fi
 
 ############## CREATE NAMED GRAPH STRUCTURE AND LOAD DATA 
-if [ $PROCESS_STRUCTURE == 1 ] ; then
-   echo ">>> PROCESS_STRUCTURE unabled"
-   /bin/bash ./process/structure_process.sh
+if [ $PROCESS_INIT == 1 ] ; then
+   echo ">>> PROCESS_INIT unabled"
+   /bin/bash ./process/virtuoso_init.sh
 else
-   echo ">>> PROCESS_STRUCTURE disabled"
+   echo ">>> PROCESS_INIT disabled"
 fi
 
-############## VIRTUOSO CONFIG
-echo "[INFO] Setting 'dbp_decode_iri' registry entry to 'on'"
-run_virtuoso_cmd "registry_set ('dbp_decode_iri', 'on');"
-echo "[INFO] Setting dynamic !!!!"
-run_virtuoso_cmd "registry_set ('dbp_DynamicLocal', 'on');"
-run_virtuoso_cmd "registry_set ('dbp_lhost', ':8890');"
-run_virtuoso_cmd "registry_set ('dbp_vhost', '${DOMAIN}');"
-echo "[INFO] Setting 'dbp_domain' registry entry to ${DOMAIN}"
-run_virtuoso_cmd "registry_set ('dbp_domain', '${DOMAIN}');"
-echo "[INFO] Setting 'dbp_graph' registry entry to ${DOMAIN}"
-run_virtuoso_cmd "registry_set ('dbp_graph', '${DOMAIN}');"
-echo "[INFO] Setting 'dbp_lang' registry entry to ${DBP_LANG}"
-run_virtuoso_cmd "registry_set ('dbp_lang', '${DBP_LANG}');"
-echo "[INFO] Setting 'dbp_category' registry entry to ${DBP_CATEGORY}"
-run_virtuoso_cmd "registry_set ('dbp_category', '${DBP_CATEGORY}');"
-
-################ INSTALL LAST DBPEDIA VAD
-echo "[INFO] Installing VAD package 'dbpedia_dav.vad'"
-run_virtuoso_cmd "vad_install('/opt/virtuoso-opensource/vad/dbpedia_dav.vad', 0);"
-echo "[INFO] Installing VAD package 'fct_dav.vad'"
-run_virtuoso_cmd "vad_install('/opt/virtuoso-opensource/vad/fct_dav.vad', 0);"
-
-##### HERE WE CHANGE THE DEFAULT BEHAVIOR OF THE DESCRIBE
-# see https://community.openlinksw.com/t/how-to-change-default-describe-mode-in-faceted-browser/1691/3
-run_virtuoso_cmd "INSERT INTO DB.DBA.SYS_SPARQL_HOST VALUES ('*',null,null,null,'DEFINE sql:describe-mode \"CBD\"');"
-
-#### DATA IMPORT PLACE
-
-echo "[INFO] deactivating auto-indexing"
-run_virtuoso_cmd "DB.DBA.VT_BATCH_UPDATE ('DB.DBA.RDF_OBJ', 'ON', NULL);"
-
-echo '[INFO] Starting load process...';
-
-load_cmds=`cat <<EOF
-log_enable(2);
-checkpoint_interval(-1);
-set isolation = 'uncommitted';
-rdf_loader_run();
-log_enable(1);
-checkpoint_interval(60);
-EOF`
-run_virtuoso_cmd "$load_cmds";
 
 run_virtuoso_cmd "log_enable(2)";
 run_virtuoso_cmd "checkpoint_interval(-1)";
 
 
 ############## CHANGE GEOLOC COORD FROM TRIPLE TO BLANK NODE
-if [ $PROCESS_GEOLOC == 1 ] ; then
+if [ $PROCESS_INIT == 1 ] ; then
    echo ">>> PROCESS_GEOLOC unabled"
    /bin/bash ./process/geoloc_changes.sh
    echo "---checkpoint"
