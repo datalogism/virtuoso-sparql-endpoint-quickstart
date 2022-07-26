23301a3a56ac176da7c8fe44792a75775946984a
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-07-26 14:07:11 +0200

    Update multilingual_labels2.sh

--- dbpedia-loader/process/multilingual_labels2.sh
+++ dbpedia-loader/process/multilingual_labels2.sh
@@ -41,18 +41,3 @@ for lang in ${lang_list[@]}; do
 	fi 
 done
 
-echo "============>>>>>>>>>> DELETE NOT USED LABELS"; 
-resp_count=$(run_virtuoso_cmd "SPARQL SELECT COUNT(?S) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels> WHERE { ?S ?p ?o . FILTER NOT EXISTS { ?S a ?t } };" 
-nb_todelete=$(get_answer_nb "$resp_count");
-while [ $nb_todelete -ne 0 ]
-do
-	$(run_virtuoso_cmd "SPARQL \
-       DEFINE sql:log-enable 2  WITH <http://fr.dbpedia.org/graph/dbpedia_generic_labels> \
-       DELETE { ?S ?p ?o. } \
-       WHERE {   \
-       SELECT ?S ?p ?o FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels> WHERE { \
-       ?S ?p ?o . FILTER NOT EXISTS { ?S a ?t } }  LIMIT $limit }};");
-       resp_count=$(run_virtuoso_cmd "SPARQL SELECT COUNT(?S) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels> WHERE { ?S ?p ?o . FILTER NOT EXISTS { ?S a ?t } };" 
-       nb_todelete=$(get_answer_nb "$resp_count");
-done
-
